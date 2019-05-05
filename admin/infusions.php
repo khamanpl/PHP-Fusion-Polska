bb4b7a01e85f26f25b2ae8c11c64ef4788af2c91
Krystian <krystianek18@tlen.pl>
2019-05-05 09:53:41 +0200

    Update infusions.php

--- admin/infusions.php
+++ admin/infusions.php
@@ -1,25 +1,27 @@
 <?php
+// Official SVN Trunk Rev: 1732
 // Infusion Management
-$locale['400'] = "Infusions";
-$locale['401'] = "Infuse";
-$locale['402'] = "This item is currently infused";
-$locale['403'] = "This item cannot be infused";
-$locale['404'] = "Infused Infusions";
-$locale['405'] = "Title";
-$locale['406'] = "Version";
-$locale['407'] = "Developer";
-$locale['408'] = "Contact";
-$locale['409'] = "Email";
-$locale['410'] = "Web";
-$locale['411'] = "Defuse";
-$locale['412'] = "Defuse this infusion?";
-$locale['413'] = "Key:";
-$locale['414'] = "Not Infused";
-$locale['415'] = "Infused";
-$locale['416'] = "Upgrade Available";
-$locale['417'] = "No infusions available.";
+$locale['400'] = "Wtyczki (Infusions)";
+$locale['401'] = "Instaluj";
+$locale['402'] = "Zaznaczony element będzie zainstalowany";
+$locale['403'] = "Zaznaczony element nie mógł być zainstalowany";
+$locale['404'] = "Zainstalowane wtyczki";
+$locale['405'] = "Tytuł";
+$locale['406'] = "Wersja";
+$locale['407'] = "Autor";
+$locale['408'] = "Kontakt";
+$locale['409'] = "E-mail";
+$locale['410'] = "WWW";
+$locale['411'] = "Odinstaluj";
+$locale['412'] = "Odinstalować wtyczkę?";
+$locale['413'] = "Legenda:";
+$locale['414'] = "Nie zainstalowano";
+$locale['415'] = "Zainstalowano";
+$locale['416'] = "Dostępna aktualizacja";
+$locale['417'] = "Brak dostępnych wtyczek.";
 $locale['418'] = "Status";
-$locale['419'] = "Options";
-$locale['420'] = "Version";
-$locale['421'] = "Author";
-$locale['422'] = "Download more Addons";
+$locale['419'] = "Opcje";
+$locale['420'] = "Wersja";
+$locale['421'] = "Autor";
+$locale['422'] = "Pobierz więcej dodatków";
+?>
