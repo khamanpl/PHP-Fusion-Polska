21a2e20040fedd2023b43a67f5ccd6e8d4306097
Krystian <krystianek18@tlen.pl>
2019-05-08 10:23:38 +0200

    update quote

--- bbcodes/quote.php
+++ bbcodes/quote.php
@@ -1,4 +1,8 @@
 <?php
 $locale['bb_quote_description'] = "Cytuje zaznaczony tekst";
 $locale['bb_quote_usage'] = "cytat";
+$locale['bb_quote_expand'] = "Zobacz";
+$locale['bb_quote_collapse'] = "Ukryj";
+$locale['bb_quote'] = "Cytat";
+$locale['bb_wrote'] = "napisał/a";
 ?>
