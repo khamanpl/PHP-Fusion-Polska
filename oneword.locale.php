4da0c4f8aefd7de3fa595895926f09cb245c5e13
Krystian <krystianek18@tlen.pl>
2019-05-11 11:18:37 +0200

    Update oneword.locale
    Do sprawdzenia!!!

--- oneword.locale.php
+++ oneword.locale.php
@@ -1,65 +1,66 @@
 <?php
 // Greetings
-$locale['hello'] = "Hello!";
-$locale['goodbye'] = "Goodbye!";
-$locale['welcome'] = "Welcome";
+$locale['hello'] = "Witaj!";
+$locale['goodbye'] = "Do zobaczenia!";
+$locale['welcome'] = "Witamy";
 
 // Status
-$locale['error'] = "Error!";
-$locale['success'] = "Success!";
-$locale['enable'] = "Enable";
-$locale['disable'] = "Disable";
-$locale['no'] = "No";
-$locale['yes'] = "Yes";
-$locale['off'] = "Off";
-$locale['on'] = "On";
+$locale['error'] = "Błąd!";
+$locale['success'] = "Powodzenie!";
+$locale['enable'] = "Włącz";
+$locale['disable'] = "Wyłącz";
+$locale['no'] = "Nie";
+$locale['yes'] = "Tak";
+$locale['off'] = "Wył.";
+$locale['on'] = "Wł.";
 
 // Navigation
-$locale['next'] = "Next";
-$locale['previous'] = "Previous";
-$locale['back'] = "Back";
-$locale['forward'] = "Forward";
+$locale['next'] = "Następny";
+$locale['previous'] = "Poprzedni";
+$locale['back'] = "Wróć";
+$locale['forward'] = "Forward"; //hmm
 
 // Action
-$locale['add'] = "Add";
-$locale['save'] = "Save";
-$locale['update'] = "Update";
-$locale['remove'] = "Remove";
-$locale['search'] = "Search";
-$locale['help'] = "Help";
-$locale['register'] = "Register";
+$locale['add'] = "Dodaj";
+$locale['save'] = "Zapisz";
+$locale['update'] = "Aktualizuj";
+$locale['remove'] = "Usuń";
+$locale['search'] = "Szukaj";
+$locale['help'] = "Pomoc";
+$locale['register'] = "Rejestracja";
 $locale['ban'] = "Ban";
-$locale['reactivate'] = "Reactivate";
-$locale['user'] = "User";
-$locale['promote'] = "Promote";
+$locale['reactivate'] = "Reaktywować";
+$locale['user'] = "Użytkownik";
+$locale['promote'] = "Promuj";
 
 //Tables
 $locale['status'] = "Status";
-$locale['order'] = "Order";
+$locale['order'] = "Zamówienie";
 $locale['id'] = "ID";
-$locale['title'] = "Title";
-$locale['rights'] = "Rights";
-$locale['image'] = "Image/Icons";
-$locale['info'] = "Info";
+$locale['title'] = "Tytuł";
+$locale['rights'] = "Prawa";
+$locale['image'] = "Obraz / ikony";
+$locale['info'] = "Informacje";
 
 // Forms
-$locale['choose'] = "Please Choose One...";
-$locale['parent'] = "Create as New Parent..";
-$locale['order'] = "Item Ordering";
+$locale['choose'] = "Proszę wybrać jeden...";
+$locale['parent'] = "Utwórz jako nowe źródło ..";
+$locale['order'] = "Zamawianie przedmiotu";
 $locale['status'] = "Status";
 $locale['note'] = "Make a note of this item";
-$locale['publish'] = "Published";
-$locale['unpublish'] = "Unpublished";
-$locale['draft'] = "Draft";
-$locale['settings'] = "Settings";
-$locale['posted'] = "posted";
-$locale['in'] = "in";
-$locale['profile'] = "Profile";
-$locale['edit'] = "Edit";
-$locale['view'] = "View";
-$locale['login'] = "Log in";
-$locale['logout'] = "Logout";
-$locale['pm'] = "Private Messages";
-$locale['logged'] = "Logged in as ";
-$locale['version'] = "Version ";
-$locale['browse'] = "Browse ...";
+$locale['publish'] = "Opublikowany";
+$locale['unpublish'] = "Niepublikowane";
+$locale['draft'] = "Wersja robocza";
+$locale['settings'] = "Ustawienia";
+$locale['posted'] = "napisał";
+$locale['in'] = "w";
+$locale['profile'] = "Profil";
+$locale['edit'] = "Eydycja";
+$locale['view'] = "Zobacz";
+$locale['login'] = "Zaloguj";
+$locale['logout'] = "Wyloguj";
+$locale['pm'] = "Prywatne wiadomości";
+$locale['logged'] = "Zalogowano jako ";
+$locale['version'] = "Wersja ";
+$locale['browse'] = "Wybierz ...";
+?>
